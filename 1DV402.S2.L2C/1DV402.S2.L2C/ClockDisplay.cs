b38f2332823179a7e302c994d6da4c79716be67d
ej222ru <ej222ru@student.lnu.se>
2014-10-02 22:14:05 +0200

    started

---
+++ 1DV402.S2.L2C/1DV402.S2.L2C/ClockDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L2C
+{
+	class ClockDisplay
+	{
+		private NumberDisplay _minuteDisplay;
+		private NumberDisplay _hourDisplay;
+
+		public string Time { get; set; }
+
+
+		public ClockDisplay()
+		{
+
+		}
+
+		public ClockDisplay(int hour, int minute)
+		{
+
+		}
+
+		public ClockDisplay(string time)
+		{
+			// time comes as HH:mm
+
+
+		}
+	}
+}
---
+++ 1DV402.S2.L2C/1DV402.S2.L2C/NumberDisplay.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L2C
+{
+	class NumberDisplay
+	{
+		private int _number;
+		private int _maxNumber;
+
+		public int Number
+		{
+			get { return _number; }
+			private set { _number = value; }
+		}
+
+		public int MaxNumber
+		{
+			get { return _maxNumber; }
+			private set { _maxNumber = value; }
+		}
+
+		public NumberDisplay(int maxNumber)
+			: this(maxNumber, 0)
+		{
+		}
+
+		public NumberDisplay(int maxNumber, int number)
+		{
+			MaxNumber = maxNumber;
+			Number = number;
+		}
+
+		public override bool Equals(object obj)
+		{
+			// If parameter is null return false.
+			if (obj == null)
+			{
+				return false;
+			}
+			// If parameter cannot be cast to Point return false.
+			NumberDisplay numberDisplay = obj as NumberDisplay;
+			if ((object)numberDisplay == null)
+			{
+				return false;
+			}
+			// return true if fields match
+			return ((Number == numberDisplay.Number)
+					&&
+					(MaxNumber == numberDisplay.MaxNumber));
+		}
+
+		/// <summary>
+		/// Also implement Equals(type) for enhanced peformance according to msdn
+		/// </summary>
+		/// <param name="numberDisplay"></param>
+		/// <returns></returns>
+		public bool Equals(NumberDisplay numberDisplay)
+		{
+			// If parameter is null return false:
+			if ((object)numberDisplay == null)
+			{
+				return false;
+			}
+			// return true if fields match
+			return ((Number == numberDisplay.Number)
+					&&
+					(MaxNumber == numberDisplay.MaxNumber));
+		}
+
+
+
+
+
+	}
+}

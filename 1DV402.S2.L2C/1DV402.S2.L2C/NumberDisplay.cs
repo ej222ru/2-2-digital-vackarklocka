62a32076c5651bcd1bcbefeb6e9f76d1a59352be
ej222ru <ej222ru@student.lnu.se>
2014-10-05 20:33:44 +0200

    NumberDisplay implemented men frågetecken kring GetHashCode()

--- 1DV402.S2.L2C/1DV402.S2.L2C/NumberDisplay.cs
+++ 1DV402.S2.L2C/1DV402.S2.L2C/NumberDisplay.cs
@@ -24,24 +24,25 @@ namespace _1DV402.S2.L2C
 		}
 
 		public NumberDisplay(int maxNumber)
-			: this(maxNumber, 0)
+			: this(maxNumber,0 )
 		{
 		}
 
 		public NumberDisplay(int maxNumber, int number)
 		{
-			MaxNumber = maxNumber;
 			Number = number;
+			MaxNumber = maxNumber;
 		}
 
 		public override bool Equals(object obj)
 		{
+
 			// If parameter is null return false.
 			if (obj == null)
 			{
 				return false;
 			}
-			// If parameter cannot be cast to Point return false.
+			// If parameter cannot be cast to NumberDisplay return false.
 			NumberDisplay numberDisplay = obj as NumberDisplay;
 			if ((object)numberDisplay == null)
 			{
@@ -71,9 +72,58 @@ namespace _1DV402.S2.L2C
 					(MaxNumber == numberDisplay.MaxNumber));
 		}
 
+		public override int GetHashCode()
+		{
+			return Convert.ToInt32(ToString());
+		}
+		public void Increment()
+		{
+			if (++Number > MaxNumber)
+				Number = 0;
+		}
+		public override string ToString()
+		{
+			if (Number <10)
+				return String.Format("{0,1}", Number);
+			else
+				return String.Format("{0,2}", Number);
+		}
+		public string ToString(string format)
+		{
+			if (format.Equals("0") || format.Equals("G"))
+				return ToString();
+			else if (format.Equals("00"))
+			{
+				return String.Format("{0,2}", Number);
+			}
+			else 
+			{  
+				throw new FormatException();
+			}
+		}
 
+		public static bool operator ==(NumberDisplay a, NumberDisplay b)
+		{
+			// If both are null, or both are same instance, return true.
+			if (System.Object.ReferenceEquals(a, b))
+			{
+				return true;
+			}
 
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null))
+			{
+				return false;
+			}
+			return a.Equals(b);
+		}
+		public static bool operator !=(NumberDisplay a, NumberDisplay b)
+		{
+			return !(a == b);
+		}
 
-
+ 
+  
 	}
+
 }
--- 1DV402.S2.L2C/1DV402.S2.L2C/Program.cs
+++ 1DV402.S2.L2C/1DV402.S2.L2C/Program.cs
@@ -12,4 +12,34 @@ namespace _1DV402.S2.L2C
 		{
 		}
 	}
+	/*
+			static void test
+			{
+				int testNumber1 = 1;
+				int testNumber2 = 1;
+				int count = 1;
+				NumberDisplay MyNumberDisplay = new NumberDisplay(12, testNumber1);
+				NumberDisplay AnotherDisplay = new NumberDisplay(12, testNumber2);
+				do
+				{
+
+
+					if (MyNumberDisplay.Equals(AnotherDisplay))
+						Console.WriteLine(string.Format("Likadana object {0} {1}", MyNumberDisplay.Number, AnotherDisplay.Number));
+					else
+						Console.WriteLine("Olika object {0} {1}", MyNumberDisplay.Number, AnotherDisplay.Number);
+
+					Console.WriteLine(string.Format("GetHashCode {0} : {1}", MyNumberDisplay.GetHashCode(), AnotherDisplay.GetHashCode()));
+					Console.WriteLine("ToString() " + MyNumberDisplay.ToString() + " : " + AnotherDisplay.ToString());
+					MyNumberDisplay.Increment();
+					if ((MyNumberDisplay.Number % 2) == 0)
+						AnotherDisplay.Increment();
+					Console.WriteLine("***************************");
+					Console.WriteLine("");
+				} while (count++ < 20);
+
+			}
+		}
+	}
+		*/
 }
